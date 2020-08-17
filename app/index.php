fe517d692cf400ce0fc34d3adb2908bd14bf5f12
Robson M. Gomes <robson.mesquita56@gmail.com>
2020-08-17 19:49:44 -0300

    upgrade: Simplified environment testing
    Less code to make a more easy to use repo

--- app/index.php
+++ app/index.php
@@ -4,13 +4,15 @@ require __DIR__.'/source/core/testConnection.php';
 <html>
     <head>
         <meta charset="utf-8"/>
-        <title>PHP Dev Env</title>
-        <link rel="stylesheet" href="shared/styles.css">
+        <title>docker-lampp</title>
     </head>
     <body>
-        <?= success('PHP is Working!'); ?>
-        <?php
-            testConnection();
-        ?>
+        <!-- DB CONNECT TEST -->
+        <?php $connect = new mysqli('db', 'dev', 'devpass', 'test_db'); ?>
+        <p><?= ($connect->connect_error) ? "DB = Error" : "DB = Ok" ?></p>
+        <!-- PHPMyAdmin -->
+        <a href="http://127.0.0.1:8900">[PHPMyAdmin]</a>
+        <!-- PHP Info -->
+        <?php phpinfo(); ?>
     </body>
 </html>
--- app/source/helpers/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('CONF_DB_HOST', 'db');
-define('CONF_DB_USER', 'dev');
-define('CONF_DB_PASS', 'devpass');
-define('CONF_DB_NAME', 'test_db');
